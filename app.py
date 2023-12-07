df9f149f6a37f88287525067ed60bed4458eb121
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-06 23:19:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ db_host = os.environ["DB_HOST"]
 db_username = os.environ["DB_USERNAME"]
 db_password = os.environ["DB_PASSWORD"]
 db_name = os.environ["DB_NAME"]
+
+print(db_host)
 ca_cert_path = "./cacert.pem"
 if os.path.exists(ca_cert_path):
     print("El certificado se encuentra en la ruta: {}".format(ca_cert_path))
