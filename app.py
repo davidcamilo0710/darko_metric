13c761aa2354744388380af2aa07472d2b4f2f8b
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-07 00:32:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
-import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
 
-config = {
-    "host": "aws.connect.psdb.cloud",
-    "user": "5tdoaohudj6ik07ht98u",
-    "password": "pscale_pw_EROWJXUbgQQHA5dUQpnrD8Z0Ue5UperUDrMqjDkGo4t",
-    "database": "darko",
-    "autocommit": True,
-    "ssl_mode": "VERIFY_IDENTITY",
-    "ssl_ca": "/etc/ssl/certs/ca-certificates.crt",
-}
-
-try:
-    # Intentar establecer la conexión
-    connection = mysql.connector.connect(**config)
-
-    # Comprobar si la conexión fue exitosa
-    if connection.is_connected():
-        print("Conexión exitosa a la base de datos 'darko'")
-except Exception as e:
-    print("Error de conexión: {}".format(e))
-finally:
-    # Cerrar la conexión
-    if 'connection' in locals() and connection.is_connected():
-        connection.close()
-        print("Conexión cerrada")
+connection = MySQLdb.connect(
+  host= os.getenv("DB_HOST"),
+  user=os.getenv("DB_USERNAME"),
+  passwd= os.getenv("DB_PASSWORD"),
+  db= os.getenv("DB_NAME"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
+  
