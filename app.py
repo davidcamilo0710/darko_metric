d6714b48c20512ffd22351ac68aadee5797b65cc
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-06 23:31:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import pymysql.cursors
+import mysql.connector
 import ssl
 import os
+
 # Parámetros de conexión
 db_host = os.environ["DB_HOST"]
 db_username = os.environ["DB_USERNAME"]
@@ -13,28 +14,3 @@ if os.path.exists(ca_cert_path):
     print("El certificado se encuentra en la ruta: {}".format(ca_cert_path))
 else:
     print("El certificado NO se encuentra en la ruta: {}".format(ca_cert_path))
-# Configuración de la conexión
-ssl_context = ssl.create_default_context(cafile=ca_cert_path)
-config = {
-    "host": db_host,
-    "user": db_username,
-    "password": db_password,
-    "database": db_name,
-    "ssl": {"ssl_context": ssl_context},
-}
-
-try:
-    # Intentar establecer la conexión
-    connection = pymysql.connect(**config)
-
-    # Comprobar si la conexión fue exitosa
-    if connection.open:
-        print("Conexión exitosa a la base de datos {}".format(db_name))
-except Exception as e:
-    print("Error de conexión: {}".format(e))
-finally:
-    # Cerrar la conexión
-    if 'connection' in locals() and connection.open:
-        connection.close()
-        print("Conexión cerrada")
-
