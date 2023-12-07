8a6e8ef87df17f3cefdf938ee115af598a9ec681
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-07 00:25:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 import mysql.connector
-import ssl
-import os
 
-# Parámetros de conexión
-db_host = os.environ["DB_HOST"]
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
-db_name = os.environ["DB_NAME"]
-
-print(db_host)
-ca_cert_path = "/etc/ssl/certs/ca-certificates.crt"
-if os.path.exists(ca_cert_path):
-    print("El certificado se encuentra en la ruta: {}".format(ca_cert_path))
-else:
-    print("El certificado NO se encuentra en la ruta: {}".format(ca_cert_path))
-
-# Configuración de la conexión
 config = {
-    "host": db_host,
-    "user": db_username,
-    "password": db_password,
-    "database": db_name,
-    "port": 3306,
-    "ssl_ca": ca_cert_path,
+    "host": "aws.connect.psdb.cloud",
+    "user": "5tdoaohudj6ik07ht98u",
+    "password": "pscale_pw_EROWJXUbgQQHA5dUQpnrD8Z0Ue5UperUDrMqjDkGo4t",
+    "database": "darko",
+    "autocommit": True,
+    "ssl_mode": "VERIFY_IDENTITY",
+    "ssl_ca": "/etc/ssl/certs/ca-certificates.crt",
 }
 
 try:
@@ -31,7 +16,7 @@ try:
 
     # Comprobar si la conexión fue exitosa
     if connection.is_connected():
-        print("Conexión exitosa a la base de datos {}".format(db_name))
+        print("Conexión exitosa a la base de datos 'darko'")
 except Exception as e:
     print("Error de conexión: {}".format(e))
 finally:
