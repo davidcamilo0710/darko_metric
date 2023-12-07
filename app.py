233161998b4d91ce43b6aa68d654bafb5df6ccb7
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-06 23:35:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,27 @@ if os.path.exists(ca_cert_path):
     print("El certificado se encuentra en la ruta: {}".format(ca_cert_path))
 else:
     print("El certificado NO se encuentra en la ruta: {}".format(ca_cert_path))
+
+# Configuración de la conexión
+config = {
+    "host": db_host,
+    "user": db_username,
+    "password": db_password,
+    "database": db_name,
+    "ssl_ca": ca_cert_path,
+}
+
+try:
+    # Intentar establecer la conexión
+    connection = mysql.connector.connect(**config)
+
+    # Comprobar si la conexión fue exitosa
+    if connection.is_connected():
+        print("Conexión exitosa a la base de datos {}".format(db_name))
+except Exception as e:
+    print("Error de conexión: {}".format(e))
+finally:
+    # Cerrar la conexión
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Conexión cerrada")
