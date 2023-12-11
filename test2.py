9cd2010989c5a0460f04200f6612117c27fbd16f
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-11 14:53:05 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -12,3 +12,29 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu') 
 driver = webdriver.Chrome(options=options)
 driver.get(url_darko)
+
+try:
+    driver.find_element(By.XPATH, "//a[text()='Daily Player Per-Game Projections']").click()
+    time.sleep(2)
+    download_button = driver.find_element_by_id("download_daily")
+    download_link = download_button.get_attribute('href')
+    csv_data = driver.execute_script(f"return fetch('{download_link}').then(response => response.text());")
+    df = pd.read_csv(StringIO(csv_data))
+    # Imprimir información básica sobre el DataFrame
+    print("Información general:")
+    print(df.info())
+    
+    # Imprimir nombres de columnas
+    print("\nNombres de columnas:")
+    print(df.columns.tolist())
+    
+    # Imprimir tipos de datos de cada columna
+    print("\nTipos de datos de columnas:")
+    print(df.dtypes)
+    
+    # Imprimir estadísticas descriptivas para columnas numéricas
+    print("\nEstadísticas descriptivas para columnas numéricas:")
+    print(df.describe())
+finally:
+    # Cierra el navegador al finalizar
+    driver.quit()
