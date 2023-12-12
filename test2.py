7450050fa1b653d659897337de17c9b45644e606
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-12 17:47:29 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,7 +16,7 @@ driver.get(url_darko)
 try:
     driver.find_element(By.XPATH, "//a[text()='Daily Player Per-Game Projections']").click()
     time.sleep(2)
-    download_button = driver.find_element_by_id("download_daily")
+    download_button = driver.find_element(By.ID, "download_daily")
     download_link = download_button.get_attribute('href')
     csv_data = driver.execute_script(f"return fetch('{download_link}').then(response => response.text());")
     df = pd.read_csv(StringIO(csv_data))
