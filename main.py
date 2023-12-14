9c893b7cf017da315af1a07c6f3068298080305f
David Muñoz Garcia <davidcamilo0710@gmail.com>
2023-12-13 21:48:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from database_manager import DatabaseManager
+from datetime import date
 
 # Definir parámetros de conexión
 token = os.environ["BOT_TOKEN"]
@@ -23,7 +24,7 @@ max_projection_date = db_manager.get_max_projection_date()
 # Insertar datos de prueba (puedes definir tus propios datos aquí)
 player_stats = (
     1, "Jugador1", 1, "Equipo1", "Experiencia1", 30.5, 100.0, 20.5, 5.5, 8.0,
-    2.5, 1.0, 1.5, 2.0, 15.0, 10.0, 5.0, 7.0, 3.0, 4.0, max_projection_date
+    2.5, 1.0, 1.5, 2.0, 15.0, 10.0, 5.0, 7.0, 3.0, 4.0, date(max_projection_date)
 )
 db_manager.insert_player_stats(player_stats)
 
